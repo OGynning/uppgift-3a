ec0da5f25bd17553f603f1fa5b9fb8c38d0a5365
OGynning <113348004+OGynning@users.noreply.github.com>
2023-09-26 20:19:53 +0200

    Initial Commit

---
+++ uppgift1ai/uppgift1aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift1aiApp.swift
+//  uppgift1ai
+//
+//  Created by oscar gynning on 2023-09-26.
+//
+
+import SwiftUI
+
+@main
+struct uppgift1aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
